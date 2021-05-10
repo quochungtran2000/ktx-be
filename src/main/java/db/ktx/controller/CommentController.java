5433c7bfd29a27a271af7ffb18acd5292a75a53d
Quoc Hung <quochung@Hungs-Mac.local>
2021-05-10 17:28:10 +0700

    update cos

--- src/main/java/db/ktx/controller/CommentController.java
+++ src/main/java/db/ktx/controller/CommentController.java
@@ -10,8 +10,8 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/cmt")
-//@CrossOrigin
 public class CommentController {
 
     @Autowired
--- src/main/java/db/ktx/controller/UserController.java
+++ src/main/java/db/ktx/controller/UserController.java
@@ -19,6 +19,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/user")
 public class UserController {
 
